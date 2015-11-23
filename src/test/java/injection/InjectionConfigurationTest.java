ae29e7f6bdab6f17e6db7eed26fa879bac3a57db
LeeDongHo <intoday87@LeeDongHoui-Mac-mini.local>
2015-11-23 22:48:47 +0900

    add sout test method

--- src/test/java/injection/InjectionConfigurationTest.java
+++ src/test/java/injection/InjectionConfigurationTest.java
@@ -1,11 +1,26 @@
 package injection;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import soundsystem.BlankDisc;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.fest.assertions.Assertions.assertThat;
+
 public class InjectionConfigurationTest{
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
     @Test
     public void test() throws Exception {
         ApplicationContext context = new AnnotationConfigApplicationContext(InjectionConfiguration.class);
@@ -13,5 +28,6 @@ public class InjectionConfigurationTest{
         BlankDisc compactDisc = context.getBean("compactDisc", BlankDisc.class);
 
         compactDisc.play();
+        assertThat("Playing title by artist").isEqualTo(outContent.toString());
     }
 }
