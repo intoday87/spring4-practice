94970cad73de59f6440e7cbbb90fadf5e741e1bc
이동호(Nick) <intoday87@coupang.com>
2016-04-24 21:43:19 +0900

    add aspectj notations

--- src/main/java/aop/Audience.java
+++ src/main/java/aop/Audience.java
@@ -6,7 +6,7 @@ import org.aspectj.lang.annotation.Before;
 @Aspect
 public class Audience {
 
-    @Before("execution(** aop.Performance.perform(..))")
+    @Before("execution(* aop.Performance.perform(..)) && within(aop.*) && bean('performanceImpl'))")
     public void silenceCellPhones() {
         System.out.println("Silence cell phones");
     }
--- src/test/java/aop/PerformanceTest.java
+++ src/test/java/aop/PerformanceTest.java
@@ -3,8 +3,11 @@ package aop;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.fest.assertions.Assertions.assertThat;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = Config.class)
@@ -15,5 +18,15 @@ public class PerformanceTest {
     @Test
     public void name() throws Exception {
         sut.perform();
+    }
+
+    @Test
+    public void beanId() throws Exception {
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(Config.class);
+
+        String beanName = "performanceImpl";
+        Object bean = applicationContext.getBean(beanName, Performance.class);
+
+        assertThat(bean).isNotNull();
     }
 }
