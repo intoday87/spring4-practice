b9891a5ad61d370739a198703c3d5bfed421d31b
이동호(Nick) <intoday87@coupang.com>
2018-07-12 21:39:38 +0900

    @Pointcut @annotation 사용

--- src/main/java/aop/Audience.java
+++ src/main/java/aop/Audience.java
@@ -1,12 +1,32 @@
 package aop;
 
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 
 @Aspect
 public class Audience {
+    @Pointcut("execution(void aop.Performance.perform(..))")
+    public void performance() {}
+
+    @Before("performance() && @annotation(aop.inner.MyAnnotation)")
+    public void beforePerformanceImpl() {
+        System.out.println("before performanceAnotherImpl");
+    }
+
     @Before("execution(void aop.Performance.perform(..))")
     public void silenceCellPhones() {
         System.out.println("Silence cell phones");
+    }
+
+    @After("execution(void aop.Performance.perform(..)) && bean(performanceAnotherImpl)")
+    public void afterPerformanceAnotherImpl() {
+        System.out.println("end of performanceAnotherImpl");
+    }
+
+    @After("execution(void aop.Performance.perform(..)) && bean(performanceImpl)")
+    public void afterPerformanceImpl() {
+        System.out.println("end of performanceImpl");
     }
 }
--- src/main/java/aop/inner/PerformanceAnotherImpl.java
+++ src/main/java/aop/inner/PerformanceAnotherImpl.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class PerformanceAnotherImpl implements Performance{
+
+    @MyAnnotation
     public void perform(String str) {
         System.out.println("perform another. " + str);
     }
